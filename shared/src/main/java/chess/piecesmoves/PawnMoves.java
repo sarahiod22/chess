48a01cbbf0170f58239d11e5f572342bbd5239e3
Sarahi <psod2@byu.edu>
2024-05-05 21:29:48 -0600

    Implemented promotion part for PawnMoves class, has bugs

--- shared/src/main/java/chess/piecesmoves/PawnMoves.java
+++ shared/src/main/java/chess/piecesmoves/PawnMoves.java
@@ -16,15 +16,17 @@ public class PawnMoves {
         if (board.getPiece(myPosition).getTeamColor() == ChessGame.TeamColor.WHITE){
             direction = 1;
             startPosition = 2;
-            promoPosition = 8;
+            promoPosition = 7;
         } else {
             direction = -1;
             startPosition = 7;
-            promoPosition = 1;
+            promoPosition = 2;
         }
 
-        possiblePositions.add(new ChessPosition(y+direction, x));
-        if (y == startPosition){
+        if (board.getPiece(new ChessPosition(y+direction, x)) == null) {
+            possiblePositions.add(new ChessPosition(y + direction, x));
+        }
+        if ((y == startPosition) && (board.getPiece(new ChessPosition(y+direction, x)) == null) && (board.getPiece(new ChessPosition(y+(2*direction), x)) == null)){
             possiblePositions.add(new ChessPosition(y+(2*direction), x));
         }
         if (board.getPiece(new ChessPosition(y+direction, x+1)) != null && (board.getPiece(myPosition).getTeamColor() != board.getPiece(new ChessPosition(y+direction, x+1)).getTeamColor())){
@@ -38,9 +40,16 @@ public class PawnMoves {
             if (position.getRow() < 1 || position.getRow() > 8 || position.getColumn() < 1 || position.getColumn() > 8)
                 continue;
 
-            if (board.getPiece(position) == null || board.getPiece(myPosition).getTeamColor() != board.getPiece(position).getTeamColor()) {
+//            if (board.getPiece(position) == null || board.getPiece(myPosition).getTeamColor() != board.getPiece(position).getTeamColor()) {
                 validPositions.add(new ChessMove(myPosition, position, null));
-            }
+            //}
+        }
+
+        if (y == promoPosition) {
+            validPositions.add(new ChessMove(myPosition, new ChessPosition(y+direction, x), ChessPiece.PieceType.QUEEN));
+            validPositions.add(new ChessMove(myPosition, new ChessPosition(y+direction, x), ChessPiece.PieceType.ROOK));
+            validPositions.add(new ChessMove(myPosition, new ChessPosition(y+direction, x), ChessPiece.PieceType.KNIGHT));
+            validPositions.add(new ChessMove(myPosition, new ChessPosition(y+direction, x), ChessPiece.PieceType.BISHOP));
         }
 
         return validPositions;
