15a36c8d26b711775a5b67a94667aea15f7e859f
Sarahi <psod2@byu.edu>
2024-05-21 16:49:46 -0600

    Implemented UserDAO

--- server/src/main/java/dataaccess/UserDao.java
+++ server/src/main/java/dataaccess/UserDao.java
@@ -8,16 +8,30 @@ public class UserDao implements IntUserDao {
 
     @Override
     public void createUser(UserData newUser) throws DataAccessException {
-
+        if (newUser.username() == null || newUser.username().isEmpty()) {
+            throw new DataAccessException("Username not provided");
+        }
+        if(users.containsKey(newUser.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(newUser.username(), newUser);
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        try {
+            return users.get(username);
+        }catch(Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        try {
+            users.clear();
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
     }
 }
