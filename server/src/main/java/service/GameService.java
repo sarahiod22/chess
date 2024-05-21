fb89b97d68ea6e00dc07e1a47c01ca7929b9615e
Sarahi <psod2@byu.edu>
2024-05-20 20:58:10 -0600

    Service Classes

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.GameData;
+
+import java.util.Collection;
+
+public class GameService {
+    public Collection<GameData> listGames(String authToken){
+        return null;
+    }
+
+    public Integer createGame(String authToken, GameData gameName){
+        return null;
+    }
+
+    public void joinGame(String authToken, String playerColor, int gameId){}
+
+    public void clear(){}
+}
---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,16 @@
+package service;
+
+import model.AuthData;
+import model.UserData;
+
+public class UserService {
+    public AuthData register(UserData user) {
+        return null;
+    }
+    public AuthData login(UserData user) {
+        return null;
+    }
+    public void logout(UserData user) {}
+
+    public void clear(){}
+}
