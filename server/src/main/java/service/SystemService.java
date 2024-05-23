2f1986b92131c2447ba64a7a79683fe98d1538dc
Sarahi <psod2@byu.edu>
2024-05-22 18:15:39 -0600

    implemented SystemService Class

--- server/src/main/java/service/SystemService.java
+++ server/src/main/java/service/SystemService.java
@@ -1,5 +1,27 @@
 package service;
 
+import dataaccess.AuthDao;
+import dataaccess.DataAccessException;
+import dataaccess.GameDao;
+import dataaccess.UserDao;
+
 public class SystemService {
-    public void clear(){}
+    private UserDao userDao;
+    private AuthDao authDao;
+    private GameDao gameDao;
+
+    public SystemService(UserDao userDao , AuthDao authDao, GameDao gameDao){
+        this.userDao = userDao;
+        this.authDao = authDao;
+        this.gameDao = gameDao;
+    }
+    public void clear() throws DataAccessException {
+        try {
+            userDao.clear();
+            authDao.clear();
+            gameDao.clear();
+        } catch (Exception e) {
+            throw new DataAccessException("Fail to clear data");
+        }
+    }
 }
