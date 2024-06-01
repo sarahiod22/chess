a3e9f88189c9d2333bb34f946e73416fa5af615f
Sarahi <psod2@byu.edu>
2024-06-01 16:56:14 -0600

    Fixed Tests. Done??

--- server/src/test/java/dataaccess/SQLAuthDaoTest.java
+++ server/src/test/java/dataaccess/SQLAuthDaoTest.java
@@ -32,15 +32,27 @@ public class SQLAuthDaoTest {
 
     @Test
     void positiveGetAuthTest() throws ResponseException {
-        authDao.createAuth(validAuthData.username());
-        AuthData auth = authDao.getAuth(validAuthData.authToken());
-        Assertions.assertNotNull(auth);
+        AuthData auth1 = authDao.createAuth(validAuthData.username());
+        AuthData auth2 = authDao.getAuth(auth1.authToken());
+        Assertions.assertNotNull(auth2);
     }
 
     @Test
     void negativeGetAuthTest() throws ResponseException {
         //user does not exist, so should return null
         Assertions.assertNull(authDao.getAuth(validAuthData.authToken()));
+    }
+
+    @Test
+    void positiveDeleteAuthTest() throws ResponseException {
+        AuthData auth = authDao.createAuth(validAuthData.username());
+        authDao.deleteAuth(auth.authToken());
+        Assertions.assertNull(authDao.getAuth(auth.authToken()));
+    }
+
+    @Test
+    void negativeDeleteAuthTest() {
+        Assertions.assertDoesNotThrow(() -> authDao.deleteAuth(invalidAuthData.authToken()));
     }
 
     @Test
